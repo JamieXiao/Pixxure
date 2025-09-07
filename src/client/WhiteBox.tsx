40236f93ad23922a346163a33f4d63b73bd415ab
Amelie Tam <atam2@students.cs.ubc.ca>
2025-09-07 14:27:35 -0700

    nes.css style added with white menu box

--- src/client/WhiteBox.tsx
+++ src/client/WhiteBox.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import 'nes.css/css/nes.min.css';
 
 export const WhiteBox = () => {
     return (
-        <div>
+        <div className="nes-container is-rounded is-centered">
             Box! 
         </div>
     );
--- src/client/menu.tsx
+++ src/client/menu.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { WhiteBox } from "./WhiteBox";
+
+import "nes.css/css/nes.min.css";
+
 
 export const Menu = () => {
     return (
-        <div>
+        // <div className="stupid-div">
+        //     <div className="nes-container is-rounded is-centered">
+        //         <h1>Menu</h1>
+        //     </div>
+        // </div>
+
+        <div className="nes-container is-rounded">
             <h1>Menu</h1>
-            <WhiteBox/>
         </div>
     );
 }
