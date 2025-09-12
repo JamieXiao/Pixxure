5f5888d3122aa8bd46b529acf80fb94a7379f492
Amelie Tam <atam2@students.cs.ubc.ca>
2025-09-11 22:16:20 -0700

    game screen components added

--- src/client/game.tsx
+++ src/client/game.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import type { Page } from "./App";
+import { Button, Card, Input } from 'pixel-retroui'
+
+// import "./index.css";
+
+
+
+
 
 type Props = {
     route: (page: Page) => void;
 }
 
 export const Game: React.FC<Props> = ({ route }) => {
+    const updateImage = () => {
+        console.log("Image updated!");
+    };
+    
     return (
-        <div>
-            Game component
-        </div>
+        <Card className="container is-rounded is-wide">
+            <div className="pix-image"></div>
+            <div className="game-bottom">
+                <img src="hearts.png" alt="hearts" id="game-hearts-img"/>
+                <Input
+                    className="guess-box"
+                    bg="#FFE3CF"
+                    textColor="#24475B"
+                    borderColor="#0"
+                    placeholder="Enter your guess here!"
+                /> 
+                <Button bg="#5A8096" textColor="white" borderColor="black" shadow="#385261ff" className="game-btn" onChange={(e) => updateImage()}>SUBMIT</Button>
+            </div>
+        </Card>
     );
 }
--- src/client/instructions.tsx
+++ src/client/instructions.tsx
@@ -15,14 +15,12 @@ export const Instructions: React.FC<Props> = ({ route }) => {
         <div className="nes-container is-rounded is-wide">
             <h1 id="how-to-play">HOW TO PLAY</h1>
             <div id="instructions-text">
-                <p>1. Guess the object in the pixelated image in the fewest tries possible</p>
+                <p>1. Guess the object in the pixelated image in the fewest tries possible (hint: the answer's only 1 word)</p>
                 <p>2. You have 5 tries - look at your hearts to see how many you have left!</p>
                 <p>3. Good luck!</p>
             </div>
             <div className="side-btns">
-                {/* <Link to="/"> */}
                 <Button colorClass="blue-btn instructions-btn" onClick={() => route("menu")}>BACK</Button>
-                {/* </Link> */}
             </div>
         </div>
     );
