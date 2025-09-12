7a125a54e6c9e92597c5c0a66561950ec79c1b81
Amelie Tam <atam2@students.cs.ubc.ca>
2025-09-11 23:07:19 -0700

    game components added

--- src/client/game.tsx
+++ src/client/game.tsx
@@ -14,10 +14,16 @@ type Props = {
 }
 
 export const Game: React.FC<Props> = ({ route }) => {
-    const updateImage = () => {
-        console.log("Image updated!");
-    };
     
+    const updateHearts = async () => {                                
+        // document.getElementById('game-hearts-img').src='heartRight2.png';
+        const img = document.getElementById("game-hearts-img");
+
+        if (img instanceof HTMLImageElement) {
+            img.src = "heartRight2.png";
+        }
+    }
+
     return (
         <Card className="container is-rounded is-wide">
             <div className="pix-image"></div>
@@ -30,7 +36,7 @@ export const Game: React.FC<Props> = ({ route }) => {
                     borderColor="#0"
                     placeholder="Enter your guess here!"
                 /> 
-                <Button bg="#5A8096" textColor="white" borderColor="black" shadow="#385261ff" className="game-btn" onChange={(e) => updateImage()}>SUBMIT</Button>
+                <Button bg="#5A8096" textColor="white" borderColor="black" shadow="#385261ff" className="game-btn" onChange={(e) => updateHearts()}>SUBMIT</Button>
             </div>
         </Card>
     );
